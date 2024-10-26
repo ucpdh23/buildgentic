45eb7241d7bfabaaa418b497675aac45b1615a46
xan xan <xan.xan@gmail.com>
2024-10-26 14:32:16 +0200

    Improving principal, removing placeholders and using dotenv

--- src/principal.py
+++ src/principal.py
@@ -2,8 +2,16 @@
 import paho.mqtt.client as mqtt
 import os
 import importlib
+from dotenv import load_dotenv
 
+load_dotenv()
+
+from mqtt_client import MqttClient
 from registry import get_registered_agents
+
+MQTT_BROKER_HOSTNAME=os.getenv("MQTT_BROKER_HOSTNAME")
+MQTT_BROKER_PORT=os.getenv("MQTT_BROKER_PORT")
+MQTT_BROKER_TOPIC=os.getenv("MQTT_BROKER_TOPIC")
 
 
 def load_agents_from_directory(directory="agents"):
@@ -16,34 +24,6 @@ def load_agents_from_directory(directory="agents"):
             importlib.import_module(module_name)
 
 
-class MqttClient:
-    def __init__(self):
-        # Initialize agents from the global registry
-        self.agents = get_registered_agents()
-        print("loaded", self.agents)
-        
-        self.client = mqtt.Client()
-
-        # Set callback for when a message is received
-        self.client.on_message = self.on_message
-
-    def connect(self, broker="localhost", port=1883, topic="test/topic"):
-        self.client.connect(broker, port)
-        self.client.subscribe(topic)
-
-    def on_message(self, client, userdata, msg):
-        message = msg.payload.decode()
-        print(f"Message received: {message}")
-
-        # Check each agent to see if it evaluates to True for the received message
-        for agent in self.agents:
-            if agent.evaluate(message):
-                agent.execute(message)
-
-    def publish(self, topic, message):
-        self.client.publish(topic, message)
-
-
 if __name__ == "__main__":
     print("starting...")
 
@@ -51,14 +31,14 @@ if __name__ == "__main__":
     load_agents_from_directory()
 
     # Create the MQTT client with the registered agents
-    mqtt_client = MqttClient()
+    mqtt_client = MqttClient(agents=get_registered_agents())
 
     # Connect and subscribe to a topic
-    mqtt_client.connect(broker="192.168.1.2", topic="test/topic")
+    mqtt_client.connect(broker=MQTT_BROKER_HOSTNAME, port=MQTT_BROKER_PORT, topic=MQTT_BROKER_TOPIC)
 
     # Example publishing a message
-    mqtt_client.publish("test/topic", "This is an alert message")
-    mqtt_client.publish("test/topic", "Short msg")
+    mqtt_client.publish(MQTT_BROKER_TOPIC, "This is an alert message")
+    mqtt_client.publish(MQTT_BROKER_TOPIC, "Short msg")
 
     # Start an infinite loop that will keep the client alive and listening
     mqtt_client.client.loop_forever()
